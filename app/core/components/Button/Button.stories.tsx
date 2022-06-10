75009029a14a06205bdbe29bb702a840f7348313
dhythm <yuta.okada.20@gmail.com>
2022-06-10 20:33:57 +0900

    update story of Button

--- app/core/components/Button/Button.stories.tsx
+++ app/core/components/Button/Button.stories.tsx
@@ -8,11 +8,11 @@ export default {
   argTypes: {},
 } as ComponentMeta<typeof Button>
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const Default = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
   children: "Button",
+  disabled: false,
+  fullWidth: false,
 }
