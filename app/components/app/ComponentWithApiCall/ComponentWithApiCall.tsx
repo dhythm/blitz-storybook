c5bc94f2195d038abd4d017e1637e3b5f9c7c9e3
dhythm <yuta.okada.20@gmail.com>
2022-10-21 12:05:45 +0900

    feat(components): update ComponentWithApiCall

--- app/components/app/ComponentWithApiCall/ComponentWithApiCall.tsx
+++ app/components/app/ComponentWithApiCall/ComponentWithApiCall.tsx
@@ -1,12 +1,14 @@
-import { FC, useEffect } from "react"
+import { FC, useEffect, useState } from "react"
 
 export const ComponentWithApiCall: FC = () => {
+  const [username, setUsername] = useState()
   useEffect(() => {
     const _f = async () => {
       const data = await fetch("/user")
-      console.log(await data.json())
+      const { username = "" } = await data.json()
+      setUsername(username)
     }
     _f()
   }, [])
-  return <p>Hello, world!!</p>
+  return <p>Hello, {username || "world"}!!</p>
 }
